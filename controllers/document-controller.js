8827fb3f33ade04dc36ca9d61c5baf424d548133
Dmitry <dimchik843@gmail.com>
2024-05-05 17:49:37 +0300

    8

--- controllers/document-controller.js
+++ controllers/document-controller.js
@@ -1,43 +1,55 @@
 const axios = require('axios')
+const moment = require('moment-timezone');
+const bizSdk = require('facebook-nodejs-business-sdk');
 
-const CAPIModel = require('../models/capiModel')
+const Content = bizSdk.Content;
+const CustomData = bizSdk.CustomData;
+const DeliveryCategory = bizSdk.DeliveryCategory;
+const EventRequest = bizSdk.EventRequest;
+const UserData = bizSdk.UserData;
+const ServerEvent = bizSdk.ServerEvent;
 
 class DocumentController {
-    // Загрузка исходной таблицы
+    
     async downloadFile(req, res, next) {
         try {
-            req.body.headers = req.headers;
-            req.body.userAgent = req.useragent;
 
-            const info = new CAPIModel(req.body);
+            const access_token = 'EAAb9l8mZCJDYBOZCnWv6UrNEQIAjp9GH4tAiaIrG1ZAdXD0PWe8hGP3Y9xgUfFJZC57pzZCqWjoWxmHbJZCo6L1CRfMn8bcfIsOnSTYqC3ngElKveTZAuZAhmH956VGyBdZA8DoUKskVV4okpN4B2RBPCFcYoio0B1lWyYPY7m2p6xeHPHITZClRIz1I3jyx23ITrONgZDZD';
+            const pixel_id = '1096596584745009';
+            const api = bizSdk.FacebookAdsApi.init(access_token);
+            
+            let current_timestamp = moment().tz('Etc/GMT+3').unix();
 
-            const sendForCAPI = {
-                data: [],
-                test_event_code: 'TEST92150',
-            };
+            const userData = (new UserData())
+                .setClientIpAddress(req.connection.remoteAddress)
+                .setClientUserAgent(req.headers['user-agent'])
+                .setFbp(req.body.fbp)
+                .setFbc(req.body.fbc);
 
-            sendForCAPI.data.push(info);
+            const serverEvent = (new ServerEvent())
+                            .setEventName('PageView')
+                            .setEventId(req.body.event_id)
+                            .setEventTime(current_timestamp)
+                            .setUserData(userData)
+                            .setEventSourceUrl(req.body.event_source_url)
+                            .setActionSource('website');
 
-            console.log(info.user_data.client_user_agent);
+            const eventsData = [serverEvent];
+            const eventRequest = (new EventRequest(access_token, pixel_id))
+                            .setEvents(eventsData)
+                            .setTestEventCode('TEST92150');
 
-            const PIXEL_TOKEN = 'EAAb9l8mZCJDYBOZCnWv6UrNEQIAjp9GH4tAiaIrG1ZAdXD0PWe8hGP3Y9xgUfFJZC57pzZCqWjoWxmHbJZCo6L1CRfMn8bcfIsOnSTYqC3ngElKveTZAuZAhmH956VGyBdZA8DoUKskVV4okpN4B2RBPCFcYoio0B1lWyYPY7m2p6xeHPHITZClRIz1I3jyx23ITrONgZDZD';
-            const ID_PIXEL = '1096596584745009';
-            const URL = `https://graph.facebook.com/v19.0/${ID_PIXEL}/events`;
-
-            axios.post(URL, sendForCAPI, {
-                params: {access_token: PIXEL_TOKEN},
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-            .then(() => {
-                console.log('Успешный ответ от сервера');
+            eventRequest.execute().then(
+            response => {
+                console.log(eventRequest);
+                console.log(eventRequest._events[0]._user_data);
                 res.status(200).json({status: 'Ok'})
-            })
-            .catch((err) => {
-                console.log('Ошибка запроса:', err.message);
+            },
+            err => {
+                console.error('Error: ', err);
                 res.status(500).json({status: 'Err'})
-            });
+            }
+            );
 
         } catch (error) {
             console.log(error.message)
---
+++ controllers/document-controller_orig.js
@@ -0,0 +1,49 @@
+const axios = require('axios')
+
+const CAPIModel = require('../models/capiModel')
+
+class DocumentController {
+    // Загрузка исходной таблицы
+    async downloadFile(req, res, next) {
+        try {
+            req.body.headers = req.headers;
+            req.body.userAgent = req.useragent;
+
+            const info = new CAPIModel(req.body);
+
+            const sendForCAPI = {
+                data: [],
+                test_event_code: 'TEST92150',
+            };
+
+            sendForCAPI.data.push(info);
+
+            console.log(info.user_data.client_user_agent);
+
+            const PIXEL_TOKEN = 'EAAb9l8mZCJDYBOZCnWv6UrNEQIAjp9GH4tAiaIrG1ZAdXD0PWe8hGP3Y9xgUfFJZC57pzZCqWjoWxmHbJZCo6L1CRfMn8bcfIsOnSTYqC3ngElKveTZAuZAhmH956VGyBdZA8DoUKskVV4okpN4B2RBPCFcYoio0B1lWyYPY7m2p6xeHPHITZClRIz1I3jyx23ITrONgZDZD';
+            const ID_PIXEL = '1096596584745009';
+            const URL = `https://graph.facebook.com/v19.0/${ID_PIXEL}/events`;
+
+            axios.post(URL, sendForCAPI, {
+                params: {access_token: PIXEL_TOKEN},
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(() => {
+                console.log('Успешный ответ от сервера');
+                res.status(200).json({status: 'Ok'})
+            })
+            .catch((err) => {
+                console.log('Ошибка запроса:', err.message);
+                res.status(500).json({status: 'Err'})
+            });
+
+        } catch (error) {
+            console.log(error.message)
+            res.status(500).json({status: 'Err'})
+        }
+    }
+}
+
+module.exports = new DocumentController();
